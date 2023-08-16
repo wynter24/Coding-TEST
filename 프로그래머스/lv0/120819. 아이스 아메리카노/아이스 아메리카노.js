b39f5299100868f8b003ad2653fbfda7ced8b1cc
wynter24 <97824352+wynter24@users.noreply.github.com>
2023-08-16 17:09:14 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,7 +1,7 @@
 function solution(money) {
     var answer = [];
-    var max_cup = Math.floor(money / 5500);
-    var change = money % 5500;
+    let max_cup = Math.floor(money / 5500);
+    let change = money % 5500;
     answer.push(max_cup, change);
     
     return answer;
