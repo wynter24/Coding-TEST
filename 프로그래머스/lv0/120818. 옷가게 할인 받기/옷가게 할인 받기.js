cd3dcf4fc84c7a3f8bfc22e16e265d45b3ef302f
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-17 15:51:18 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,30 +1,21 @@
-// function solution(price) {
-//     if(price >= 500000) {
-//         price *= 0.8;
-//     } else if(price >= 300000) {
-//         price *= 0.9;
-//     } else if(price >= 100000) {
-//         price *= 0.95;
-//     }
-//     return Math.floor(price);
-// }
-// function solution(price) {
-//     if(100000 <= price && price< 300000) {
-//         return Math.floor(price*0.95);
-//     } else if(300000<=price && price<500000) {
-//         return Math.floor(price*0.9);
-//     } else  {
-//         return Math.floor(price*0.8);
-//     }
-// }
 function solution(price) {
-    if (price >= 100000 && price < 300000) {
-        return Math.floor(price * .95)
-    } else if (price >= 300000 && price < 500000) {
-        return Math.floor(price * .9)
-    } else if (price >= 500000) {
-        return Math.floor(price * .8)
-    } else {
-        return price
+    if(price >= 500000) {
+        price *= 0.8;
+    } else if(price >= 300000) {
+        price *= 0.9;
+    } else if(price >= 100000) {
+        price *= 0.95;
     }
+    return Math.floor(price);
 }
+// function solution(price) {
+//     if (price >= 100000 && price < 300000) {
+//         return Math.floor(price * .95)
+//     } else if (price >= 300000 && price < 500000) {
+//         return Math.floor(price * .9)
+//     } else if (price >= 500000) {
+//         return Math.floor(price * .8)
+//     } else {
+//         return price
+//     }
+// }
