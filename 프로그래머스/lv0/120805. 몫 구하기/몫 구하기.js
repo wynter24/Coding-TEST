3c5d87ccb8a2d469833b4920a57274c19c201e26
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-17 14:13:33 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -1,5 +1,5 @@
-// function solution(num1, num2) {
-//     var answer = parseInt(num1 / num2);
-//     return answer;
-// }
-const solution = (num1, num2) => Math.floor(num1 / num2);
+function solution(num1, num2) {
+    var answer = parseInt(num1 / num2);
+    return answer;
+}
+// const solution = (num1, num2) => Math.floor(num1 / num2);
