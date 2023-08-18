1bb65a79097e47fc22369ca900bb7766637eb6ef
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-18 17:28:26 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,8 @@
 function solution(arr) {
-    let sum = 0;
-    for(i=0;i<arr.length;i++) {
-        sum += arr[i];
-    }
-    return sum/arr.length;
+    // let sum = 0;
+    // for(i=0;i<arr.length;i++) {
+    //     sum += arr[i];
+    // }
+    // return sum/arr.length;
+    return arr.reduce((x,y) => x+y) / arr.length
 }
