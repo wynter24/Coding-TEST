3d7a8a41db145578ce0d4222c99d94509da022cc
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-19 01:01:17 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,21 +1,37 @@
+// function solution(arr, n) {
+//     var answer = [];
+//     if(arr.length%2===0) {
+//         for(i=0;i<arr.length;i++) {
+//             if(i%2==0){
+//                 answer.push(arr[i]);
+//             } else {
+//                 answer.push(arr[i]+n);
+//             }
+//         }
+//     } else {
+//         for(i=0;i<arr.length;i++) {
+//             if(i%2==0){
+//                 answer.push(arr[i]+n);
+//             } else {
+//                 answer.push(arr[i]);
+//             }
+//         }
+//     }
+//     return answer;
+// }
 function solution(arr, n) {
     var answer = [];
-    if(arr.length%2===0) {
-        for(i=0;i<arr.length;i++) {
-            if(i%2==0){
-                answer.push(arr[i]);
-            } else {
-                answer.push(arr[i]+n);
-            }
-        }
-    } else {
-        for(i=0;i<arr.length;i++) {
-            if(i%2==0){
-                answer.push(arr[i]+n);
-            } else {
-                answer.push(arr[i]);
-            }
+
+    if(arr.length % 2 == 1){
+        for(let i =0; i<arr.length; i+=2){
+            arr[i]+=n
         }
     }
-    return answer;
+    else{
+        for(let i =1; i<arr.length; i+=2){
+            arr[i]+=n
+        }
+    }
+
+    return arr;
 }
