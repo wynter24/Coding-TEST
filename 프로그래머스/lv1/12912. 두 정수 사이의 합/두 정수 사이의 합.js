63834e89a8c8b374fb2bf0bdb89aea84e30d3999
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-19 09:57:14 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = 0;
+    if(b>a) {
+        for(i=a;i<=b;i++) {
+        answer += i;
+        }
+    } else {
+        for(i=b;i<=a;i++) {
+        answer += i;
+        }
+    }
+    return answer;
+}
