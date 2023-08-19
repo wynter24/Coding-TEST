b4f5ef47f9f17316c13793586f2024728cac8cad
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-19 22:22:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,14 +1,15 @@
 function solution(numbers) {
-    let sum = 0;
-    for(i=0;i<numbers.length;i++) {
-        sum += numbers[i];
-    }
-    // for(i=0;i<10;i++) {
-    //     if(numbers[i] !== i) {
-    //         sum += i;
-    //         // return numbers[i];
-    //     }
-    //     // num = numbers.filter(n => n !== i);
+    // let sum = 0;
+    // for(i=0;i<numbers.length;i++) {
+    //     sum += numbers[i];
     // }
-    return 45-sum;
+    // return 45-sum;
+    
+    let sum = 0;
+      for(i=0;i<10;i++) {
+        if(!numbers.includes(i)) {
+          sum += i;
+        }
+      }
+     return sum;
 }
