b20cfa0e0c188a7949c026f9ba45032a1b3fdab5
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-22 00:18:36 +0900

    [level 1] Title: 행렬의 덧셈, Time: 14.81 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,7 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    for (i=0;i<arr1.length; i++) {
+      answer.push(arr1[i].map((x, y) => arr1[i][y] + arr2[i][y]));
+    }
+    return answer;
+}
