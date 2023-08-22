a83afbc1912e3bf42063cf53a8628ae0a33bad79
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-08-22 14:00:45 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -4,8 +4,8 @@ function solution(n, m) {
     let least =[];
     for(i=1; i<=m; i++) {
         if(n%i==0 && m%i==0) {
-            greatest.push(i);
-            least.push(i * (n/i) * (m/i));
+            greatest.push(i); // i = n과 m의 공통 약수들 ex) 1, 3
+            least.push(i * (n/i) * (m/i)); // 12, 36 출력됨, 여기서 최소공배수
         }
     }
     answer.push(Math.max(...greatest )); // 최대공약수
