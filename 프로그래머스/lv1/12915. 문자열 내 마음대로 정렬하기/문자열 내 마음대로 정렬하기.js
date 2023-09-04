97ab706ffe6c6fa119362a59d43e413589fa6211
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-09-04 15:04:01 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,16 @@
+function solution(strings, n) {
+  var answer = [];
+  answer = strings.sort((a, b) => {
+    if (a[n] == b[n]) {
+      if (a > b) return 1;
+      if (a < b) return -1;
+      return 0;
+    }
+    if (a[n] > b[n]) {
+      return 1;
+    } else if (a[n] < b[n]) {
+      return -1;
+    }
+  });
+  return answer;
+}
