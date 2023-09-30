6010571ff1c3cb6ce3793ad761f5f46833928144
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-09-30 17:57:16 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,17 +1,3 @@
 function solution(hp) {
-    var answer = 0;
-    const head = Math.floor(hp/5)
-    const mid = Math.floor((hp%5) / 3)
-    if(hp%5 === 0) {
-        answer += head
-    } else {
-        answer += head
-        if((hp%5) % 3 === 0){
-           answer += mid
-        } else {
-            answer += mid
-            answer += (hp%5) % 3
-        }
-    }
-    return answer;
+    return Math.floor(hp/5)+Math.floor((hp%5)/3)+Math.floor((hp%5)%3)
 }
