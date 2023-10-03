082af7e729b65fe11a569565070c404b585b4a70
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-10-03 16:58:09 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,13 +1,8 @@
 function solution(numbers, direction) {
     if(direction==='right') {
-        let standard = numbers[numbers.length-1]
-        numbers.pop()
-        numbers.unshift(standard)
-        return numbers
+        numbers.unshift(numbers.pop())
     } else {
-        let standard = numbers[0]
-        let newArr = numbers.splice(1)
-        newArr.push(standard)
-        return newArr
+        numbers.push(numbers.shift())
     }
+    return numbers
 }
