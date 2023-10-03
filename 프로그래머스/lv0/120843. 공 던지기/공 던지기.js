abfb40d8941f0a9b0d3cbc5e2a43aef73d645463
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-10-03 16:13:42 +0900

    [level 0] Title: 공 던지기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,13 @@
+function solution(numbers, k) {
+    let count = 0;
+    for(let i=0;i=>0;i+=2) {
+        if(i>numbers.length){
+            i = i-numbers.length
+            numbers[i]
+        }
+        count++
+        if(count === k) {
+           return numbers[i] 
+        }
+    }
+}
