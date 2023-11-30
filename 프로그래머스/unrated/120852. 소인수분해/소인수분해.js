8f1801706243fdab3c56a5548a83cc89d6c5e958
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-12-01 00:01:04 +0900

    [level unrated] Title: 소인수분해, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.js
@@ -1,14 +1,10 @@
 function solution(n) {
     var answer = [];
     for(i=2;i<=n;i++) {
-        if(n%i==0) {
+        while(n%i==0) {
+            n = parseInt(n/i); 
             answer.push(i);
-            for(j=n;j>0;parseInt(j/=i)) {
-                if(j%i==0) {
-                    n = parseInt(j/i); 
-                }
-            }
-        }  
+        } 
     }
-    return answer;
+    return [...new Set(answer)];
 }
