1fd0b8ed5ed04a5d36579d45fe27f77d2d37767c
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2023-12-11 23:51:24 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,8 @@
 function solution(strlist) {
-    var answer = [];
-    for(i=0;i<strlist.length;i++) {
-        answer.push(strlist[i].length)
-    }
-    return answer;
+    // var answer = [];
+    // for(i=0;i<strlist.length;i++) {
+    //     answer.push(strlist[i].length)
+    // }
+    // return answer;
+    return strlist.map(item => item.length)
 }
