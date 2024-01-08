c1e742ef59dc7d255a86dc44d7fd6bcc02c3886b
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-01-08 23:07:02 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var answer = '';
+    let capital = my_string.toUpperCase();
+    let lowercase = my_string.toLowerCase();
+    for(let i=0; i<my_string.length;i++){
+        if(my_string[i] !== capital[i]) {
+            answer += capital[i]
+        } else {
+            answer += lowercase[i]
+        }
+    }
+    return answer;
+}
