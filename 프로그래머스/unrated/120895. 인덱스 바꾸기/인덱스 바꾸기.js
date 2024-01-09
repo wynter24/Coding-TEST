3ccc85cf9222ca00b466763f89be37c7aec89af5
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-01-09 23:54:07 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,7 +1,6 @@
 function solution(my_string, num1, num2) {
+// 구조 분해 할당 : 배열이나 객체의 속성을 해체하여 그 값을 개별 변수에 담는다.
     let newArr = my_string.split('');
-    let originNum1 = newArr[num1];
-    newArr[num1] = newArr[num2];
-    newArr[num2] = originNum1
+    [newArr[num1], newArr[num2]] = [newArr[num2], newArr[num1]];
     return newArr.join('');
 }
