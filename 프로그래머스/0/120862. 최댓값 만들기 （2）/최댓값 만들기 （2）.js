c14b2111b714f95fe53296fafb73cba33959250c
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-01-22 16:14:27 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,9 +1,10 @@
 function solution(numbers) {
-    var answer = [];
-    for(i=0;i<numbers.length;i++) {
-        for(j=i+1;j<numbers.length;j++) {
-            answer.push(numbers[i]*numbers[j])
-        }
-    }
-    return Math.max(...answer);
+    let l = numbers.length;
+    
+    let newarr = numbers.sort((a,b) => a-b);
+    
+    return Math.max(
+        newarr[0] * newarr[1],
+        newarr[l-1] * newarr[l-2]
+    )
 }
