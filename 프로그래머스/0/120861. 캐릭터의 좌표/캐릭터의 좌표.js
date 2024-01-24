2054abd549239021d4414ce7443dddfb07283155
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-01-25 00:19:08 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,19 +1,12 @@
 function solution(keyinput, board) {
-    // x: right/left , y: up/down
-    let x = 0;
-    let y = 0;
-    
-    keyinput.forEach(dir => {
-        if(dir === 'left' && -(board[0]-1)/2<x) {
-            x -= 1
-        } else if(dir === 'right' && x<(board[0]-1)/2) {
-            x += 1
-        } else if(dir === 'up' && y<(board[1]-1)/2) {
-            y += 1
-        } else if(dir === 'down' && -(board[1]-1)/2<y) {
-            y -= 1
+    let res = [0,0];
+    for(let p of keyinput) {
+        switch(p) {
+            case 'left': if(-res[0] < (board[0]-1)/2) res[0]--; break;
+            case 'right': if(res[0] < (board[0]-1)/2) res[0]++; break;
+            case 'up': if(res[1] < (board[1]-1)/2) res[1]++; break;
+            case 'down': if(-res[1] < (board[1]-1)/2) res[1]--; break;
         }
-    })
-    
-    return [x,y];
+    }
+    return res;
 }
