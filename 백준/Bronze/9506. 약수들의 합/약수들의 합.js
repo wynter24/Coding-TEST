b81ab7ff21b8f61480ae798b587a4a4bba65f973
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-03-02 23:24:20 +0900

    [Bronze I] Title: 약수들의 합, Time: 228 ms, Memory: 10336 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(str => str.trim());
+
+input.forEach(el => {
+  let arr = [];
+  el = Number(el);
+    
+  if(el < 0) return;
+  for (let i = 1; i <= el; i++) {
+    if(el%i === 0) arr.push(i);    
+  }
+    
+  arr.pop();
+  let sumofArr = arr.reduce((a,c) => a+c);
+    
+  if(sumofArr === el) {
+    console.log(`${el} = ${arr.join(' + ')}`)
+  } else {
+    console.log(`${el} is NOT perfect.`)
+  }
+})
