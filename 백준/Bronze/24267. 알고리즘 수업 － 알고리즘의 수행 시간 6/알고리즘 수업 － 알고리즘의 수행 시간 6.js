6489238bc14f23092803f6ac7ec24bdb216a4e0d
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-03-18 21:54:46 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const result = (BigInt(input) * BigInt(input-1) * BigInt(input-2)) / BigInt(6)
+
+console.log(`${result}\n${3}`);
