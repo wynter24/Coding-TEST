1e138db7cec3a524ddbf2dd4bad6602b4ec1f8bf
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-04-19 20:10:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.59 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    let answer = [];
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] === arr[i+1]) continue;
+        else answer.push(arr[i]);
+    }
+    return answer;
+}
