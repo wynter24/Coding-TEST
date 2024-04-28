186f2d19eae03544ae9bad5ca3f2259b03b75a57
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-04-28 22:51:23 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    for(const card of goal) {
+        if(cards1[0] === card) {
+            cards1.shift();
+        } else if(cards2[0] === card) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
+    }
+    
+    return "Yes";
+}
