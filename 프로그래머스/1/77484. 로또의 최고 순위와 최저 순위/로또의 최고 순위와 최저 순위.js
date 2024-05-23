4fd5afaded8a4074d07f4027e1faf3830b6eec6f
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-05-23 22:33:54 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,16 +1,8 @@
 function solution(lottos, win_nums) {
-  let sameNums = [6,5,4,3,2];
-  let sameArr = [];
-  let sameCnt = 0;
-  let zeroCnt = 0;
+  let rank = [6,6,5,4,3,2,1];
 
-  for (let i = 0; i < lottos.length; i++) {
-    if(win_nums.includes(lottos[i])) sameCnt++;
-    if(lottos[i] === 0) zeroCnt++;
-  }
-  sameArr.push(sameCnt + zeroCnt);
-  sameArr.push(sameCnt);
-  let answer = sameArr.map(el => el < 2 ?  6 : sameNums.indexOf(el) + 1)
+  let min = lottos.filter(el => win_nums.includes(el)).length;
+  let max = lottos.filter(el => !el).length + min;
 
-  return answer;
+  return [rank[max], rank[min]];
 }
