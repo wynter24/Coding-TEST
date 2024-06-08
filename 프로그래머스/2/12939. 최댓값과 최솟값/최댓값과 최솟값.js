8ecb25cf0f56d3ef7d28eb1d34992516f7ce800d
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-06-08 11:37:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,5 @@
 function solution(s) {
-    var answer = '';
-    s = s.split(' ').sort((a,b) => a-b);
-    answer += `${s[0]} `;
-    answer += s[s.length-1];
-    return answer;
+    const arr = s.split(' ');
+    
+    return Math.min(...arr)+' '+Math.max(...arr);
 }
