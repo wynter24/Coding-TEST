44f287d13153d757d985fdca738b0e6ce63be25a
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-06-13 13:27:22 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  let dp = new Array(n+1);
+  dp[1] = 1; dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+  }
+
+  return dp[n];
+}
