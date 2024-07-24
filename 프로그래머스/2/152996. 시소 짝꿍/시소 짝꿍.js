6fd4647cee5888f0dc47041bc819948f395dc9dc
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-07-24 16:19:44 +0900

    [level 2] Title: 시소 짝꿍, Time: 60.93 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,13 +1,19 @@
 function solution(weights) {
-  const map = {};
-  const ratio = [1, 3 / 2, 4 / 3, 2]; 
+  // 등장 횟수 map
+  let map = {};
+  // 거리: 동일, 2 & 3, 3 & 4, 2 & 2 비율
+  let ratio = [1,3/2,4/3,2];
 
+  // 무게를 내림차순 정렬 후 reduce로 합산
   return weights
-    .sort((a, b) => b - a)
+    .sort((a,b) => b-a)
     .reduce((result, weight) => {
-      ratio.map((v) => (result += map[weight * v] || 0)); 
-    
-      map[weight] = (map[weight] || 0) + 1; 
-      return result;
-    }, 0);
+      // 해당 무게의 비율을 곱하여 result에 합산
+      // 짝무게(무게*비율)가 map에 존재하면 등장횟수만큼 결과에 더한다.
+      ratio.map(v => result += (map[weight*v] || 0))
+      
+      // 해당 무게의 등장 횟수 1 증가
+      map[weight] = (map[weight] || 0) + 1;
+      return result;      
+    },0)
 }
