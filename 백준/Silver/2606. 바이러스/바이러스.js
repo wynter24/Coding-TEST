4450fd3ca060b2bdef9d2deb357821cd2d2cbc64
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-08-14 11:25:43 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 9412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -3,8 +3,7 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n
 let n = +input[0];
 let arr = input.slice(2,).map(v => v.split(' ').map(Number));
 let graph = Array.from({ length: n + 1 }, () => []);
-let visited = Array(n + 1).fill(0);
-let order = 1;
+let visited = Array(n + 1).fill(false);
 
 arr.forEach(([from, to]) => {
   graph[from].push(to);
@@ -12,14 +11,14 @@ arr.forEach(([from, to]) => {
 });
 
 function dfs(node) {
-  if (!visited[node]) {
-    visited[node] = order++;
-    for (let next of graph[node]) dfs(next);
-  }
+  visited[node] = true;
+  let count = 1;
+  graph[node].forEach(next => {
+    if (!visited[next]) {
+      count += dfs(next);
+    }
+  });
+  return count;
 }
 
-dfs(1);
-
-const answer = visited.slice(1).filter(v => v !== 0).length;
-
-console.log(answer - 1);
+console.log(dfs(1) - 1);
