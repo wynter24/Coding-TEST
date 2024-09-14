2c3c16461b4ae2a6d3e10e47dfe0db6c6533f10e
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-09-14 10:47:00 +0900

    [Silver I] Title: 회의실 배정, Time: 332 ms, Memory: 43512 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -1,23 +1,17 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-let num = Number(input.shift());
+let N = +input.shift();
+let timeTable = input.map(table => table.split(' ').map(Number));
+timeTable.sort((a, c) => a[1] === c[1] ? a[0] - c[0] : a[1] - c[1]);
 
-let arr = [];
-for (let i = 0; i < num; i++) {
-    arr.push(input[i].split(' ').map(Number))
+let count = 1;
+let curE = timeTable[0][1];
+
+for (let i = 1; i < N; i++) {
+  const [nextS, nextE] = timeTable[i];
+  if (curE <= nextS) {
+    curE = nextE;
+    count++;
+  }
 }
 
-
-arr = arr.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]);
-
-let result = 1;
-let end = arr[0][1];
-
-for (let i = 1; i < arr.length; i++) {
-    let [from, to] = arr[i];
-    if (from >= end) {
-        end = to;
-        result++;
-    }
-}
-
-console.log(result)
+console.log(count);
