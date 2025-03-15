4b2ecc534e9f81d2d1eebd07581b9734832855be
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-03-15 14:44:35 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.js
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -1,7 +1,7 @@
 let num = +require('fs').readFileSync('/dev/stdin').toString().trim();
 
 function factorial(num) {
-  if (num === 0) return 1;
+  if (num <= 1) return 1;
   return num * factorial(num - 1);
 }
 
