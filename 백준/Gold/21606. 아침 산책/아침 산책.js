5c39b86317777714f05d4ac44e4521063cc465a1
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-05-09 17:00:32 +0900

    [Gold III] Title: 아침 산책, Time: 1124 ms, Memory: 139992 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.js
+++ 백준/Gold/21606. 아침 산책/아침 산책.js
@@ -7,37 +7,35 @@ let input = require('fs')
 const N = +input.shift();
 const INOUT = input.shift().split('').map(Number);
 const trees = Array.from({ length: N + 1 }, () => []);
+let visited = Array(N + 1).fill(false);
+let count = 0;
 
 input.forEach(el => {
   const [from, to] = el.trim().split(' ').map(Number);
   trees[from].push(to);
   trees[to].push(from);
+
+  if (INOUT[from - 1] === 1 && INOUT[to - 1] === 1) count += 2;
 })
 
-let count = 0;
+function dfs(node) {
+  visited[node] = true;
+  let indoorCount = 0;
 
-function countWalkPaths(start) {
-  let k = 0;
-
-  for (const next of trees[start]) {
-    if (INOUT[next - 1] === 1) {
-      k++;
-    }
+  for (const next of trees[node]) {
+    if (visited[next]) continue;
+    if (INOUT[next - 1] === 1) indoorCount++;
+    else indoorCount += dfs(next);
   }
 
-  return k * (k - 1);
+  return indoorCount;
 }
 
 for (let i = 1; i <= N; i++) {
-  if (INOUT[i - 1] === 1) {
-    for (const next of trees[i]) {
-      if (INOUT[next - 1] === 1) {
-        count++;
-      }
-    }
-  } else {
-    count += countWalkPaths(i);
+  if (!visited[i] && INOUT[i - 1] === 0) {
+    const indoorCount = dfs(i);
+    count += indoorCount * (indoorCount - 1);
   }
 }
 
-console.log(count)
+console.log(count);
