77332a49a8ff3f82274ef3b1d375f301c0a997a1
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-05-17 11:00:25 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,16 +1,17 @@
 function solution(progresses, speeds) {
-  let answer = [0];
-  let days = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
-  let maxDay = days[0];
-
-  for (let i = 0, j = 0; i < days.length; i++) {
-    if(days[i] <= maxDay) {
-      answer[j]++; 
-    } else {
-      maxDay = days[i];
-      answer[++j] = 1;
+    let deploy = [];
+    for(let i = 0; i < speeds.length; i++) {
+        const work = Math.ceil((100 - progresses[i]) / speeds[i]);
+        if(deploy.length <= 0) {
+            deploy.push([work, 1]);
+        } else {
+            let prev = deploy[deploy.length - 1];
+            if (work <= prev[0]) prev[1]++;
+            else deploy.push([work, 1]);
+        }
     }
-  }
-
-  return answer;
+    
+    const answer = deploy.map(([day, cnt]) => {return cnt});
+    
+    return answer;
 }
