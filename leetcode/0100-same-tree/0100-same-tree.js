76f31dd679de0b111573bca85d682c37d43b7a58
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-05-23 10:06:35 +0900

    Time: 0 ms (100%), Space: 53.9 MB (33.34%) - LeetHub

---
+++ leetcode/0100-same-tree/0100-same-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if(!q && !p) {
+        return true;
+    }
+
+    if(p && q && p.val === q.val){
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+    
+    return false;
+};
