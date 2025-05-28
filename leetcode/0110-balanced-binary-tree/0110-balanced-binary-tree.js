e99fce55f3b6b959fc7889496e83097ea1162684
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-05-28 11:13:56 +0900

    Time: 1 ms (89.2%), Space: 58.9 MB (92.48%) - LeetHub

---
+++ leetcode/0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    const getHeight = (node) => {
+        if(!node) return 0;
+
+        const left = getHeight(node.left);
+        const right = getHeight(node.right);
+
+        if(left === -1 || right === -1) return -1;
+        if(Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+    
+    return getHeight(root) !== -1;
+};
