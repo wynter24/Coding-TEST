5c103bd920093a32990477d67ca65dc9f95ff419
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-07-07 15:03:24 +0900

    [level 1] Title: 모의고사, Time: 4.39 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,17 +1,17 @@
 function solution(answers) {
-  let answer = [];
-  let score = [];
-  let supo = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
-  
-  for(let i=0; i<supo.length; i++){
-    score[i] = answers.filter((v,idx) => 
-      v === supo[i][idx % supo[i].length]).length
-  }
+    const pattern1 = [1, 2, 3, 4, 5];
+    const pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let cntArray = [0, 0, 0];
+    let result = [];
 
-  let maxSupo = Math.max(...score);
-  for (let i = 0; i < score.length; i++) {
-    if(score[i] === maxSupo) answer.push(i+1);
-  }
+    for (let i = 0; i < answers.length; i++) {
+      if (answers[i] === pattern1[i % pattern1.length]) cntArray[0]++;
+      if (answers[i] === pattern2[i % pattern2.length]) cntArray[1]++;
+      if (answers[i] === pattern3[i % pattern3.length]) cntArray[2]++;
+    }
 
-  return answer;
+    let max = Math.max(...cntArray);
+    cntArray.forEach((v, i) => max === v ? result.push(i + 1) : '');
+    return result;
 }
