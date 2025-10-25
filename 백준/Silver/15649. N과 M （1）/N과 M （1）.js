7154f7c0bf785efcc40f8a5275d8d2c8a3a734eb
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-10-26 00:02:58 +0900

    [Silver III] Title: N과 M (1), Time: 184 ms, Memory: 18120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,22 +1,24 @@
 const [N, M] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 let visited = Array(N + 1).fill(false);
+let selected = [];
 let result = [];
 
 const recursive = (num) => {
   if (num === M) {
-    console.log(result.join(' '))
+    result.push(selected.join(' '))
     return;
   }
 
   for (let i = 1; i <= N; i++) {
     if (!visited[i]) {
       visited[i] = true;
-      result.push(i);
+      selected.push(i);
       recursive(num + 1);
-      result.pop();
+      selected.pop();
       visited[i] = false;
     }
   }
 }
 
 recursive(0);
+console.log(result.join(`\n`))
