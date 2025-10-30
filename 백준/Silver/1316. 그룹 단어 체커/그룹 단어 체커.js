46b2aca223f47cf3559a05bf989e90f35fb14936
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-10-30 10:47:24 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 88 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,25 +1,21 @@
 let [total, ...arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-let words = arr.map(word => word.trim().split(''));
+let count = 0;
 
-const isGroupWord = (word) => {
-  let prevList = [];
-  prevList.push(word[0]);
-
-  for (let i = 1; i < word.length; i++) {
-    const prev = prevList[prevList.length - 1];
-    if (prev !== word[i]) {
-      if (prevList.includes(word[i])) return false;
-      else prevList.push(word[i]);
-    } else {
-      continue;
+const groupCheck = (word) => {
+  let words = word.split('');
+  let stack = [words[0]];
+  for (let i = 1; i < words.length; i++) {
+    const prev = stack[stack.length - 1];
+    if (prev !== words[i]) {
+      if (stack.includes(words[i])) return false;
+      stack.push(words[i])
     }
   }
   return true;
 }
 
-let count = 0;
-words.forEach(word => {
-  if (isGroupWord(word)) count++;
-})
+for (let i = 0; i < total; i++) {
+  if (groupCheck(arr[i])) count++;
+}
 
 console.log(count);
