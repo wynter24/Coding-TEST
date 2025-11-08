9f3afc99917b175adf57c5555f9a93590822d586
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2025-11-08 10:59:15 +0900

    [Gold V] Title: AC, Time: 236 ms, Memory: 42164 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.js
+++ 백준/Gold/5430. AC/AC.js
@@ -1,16 +1,15 @@
 let [T, ...test] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
 let answer = [];
 
-const acTest = (P, N, arr) => {
+const acTest = (FN, LEN, arr) => {
   let isReverse = false;
   let start = 0;
-  let end = N - 1;
-
-  for (const el of P) {
-    if (el === 'R') {
-      isReverse = !isReverse;
-    } else {
-      if (start > end) return 'error';
+  let end = LEN - 1;
+  for (const el of FN) {
+    if (el === 'R') isReverse = !isReverse;
+    else {
+      if (end < start) return 'error';
       isReverse ? end-- : start++;
     }
   }
@@ -20,11 +19,11 @@ const acTest = (P, N, arr) => {
 }
 
 for (let i = 0; i < test.length; i += 3) {
-  const P = test[i].trim().split('');
-  const N = +test[i + 1];
-  const arr = JSON.parse(test[i + 2]);
+  const FN = test[i].trim().split('');
+  const LEN = +test[i + 1].trim();
+  let arr = JSON.parse(test[i + 2].trim());
 
-  answer.push(acTest(P, N, arr));
+  answer.push(acTest(FN, LEN, arr));
 }
 
 console.log(answer.join(`\n`));
