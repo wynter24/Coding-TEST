03b684dd21aaab050d9822f4e3680f6da55165a1
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2026-06-17 08:47:52 +0900

    [level 1] Title: 특정 옵션이 포함된 자동차 리스트 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -0,0 +1,9 @@
+-- '네비게이션' 옵션이 포함
+-- 자동차 ID 기준 내림차순
+SELECT CAR_ID
+    , CAR_TYPE
+    , DAILY_FEE
+    , OPTIONS
+FROM CAR_RENTAL_COMPANY_CAR 
+WHERE OPTIONS LIKE '%네비게이션%'
+ORDER BY CAR_ID DESC;
